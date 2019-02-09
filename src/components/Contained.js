781219334b81ff402ffc9e54f920036a6316a602
Nas <nassersanou23@gmail.com>
2019-02-09 15:45:03 -0500

    adds cmponent for separated render

--- src/components/Contained.js
+++ src/components/Contained.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Form from './Form';
+import Sidenav from './Sidenav';
 
 export default function Contained() {
   return (
     <div>
       <Form/>
+      <Sidenav/>
     </div>
   )
 }
