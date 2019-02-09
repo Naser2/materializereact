dd0db7ba5148638afca20c3ac6aa839823ebc469
Nas <nassersanou23@gmail.com>
2019-02-09 15:45:26 -0500

    adds changes

---
+++ src/components/PushPinHT.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PushPin from './PushPin';
+
+export default class PushPinHT extends Component {
+  render() {
+    return (
+      <div>
+        Hello Push 
+         <PushPin />
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Contained from './components/Contained';
+import PushPin from './components/PushPin';
+import PushPinHT from './components/PushPinHT';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<PushPinHT />, document.getElementById('root2'));
+ReactDOM.render( <Contained/> , document.getElementById('contained'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
