73e2477ff35b71bbe19a952393bed1c0c00ddbe4
Nas <nassersanou23@gmail.com>
2019-02-09 15:45:58 -0500

    updated AppJs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Typography from './Typography';
-import Card from './Card';
-import BreadcrumbsAndFooter from './BreadcrumbsAndFooter';
-import Form from './Form';
+import Typography from './components/Typography';
+import Card from './components/Card';
+import BreadcrumbsAndFooter from './components/BreadcrumbsAndFooter';
+import Form from './components/Form';
+import Paralax from './components/Paralax';
+import Sidenav from './components/Sidenav';
+import Scrollspy from './components/Scrollspy';
+import Tabs from './components/Tabs';
+// import PushPin from './components/PushPin'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Typography />
-        <Card/>
-        <BreadcrumbsAndFooter/>
+        <Card />
+        <BreadcrumbsAndFooter />
+        {/* <PushPin /> */}
+        <Sidenav />
+        <Paralax />
+        <Scrollspy />
+        <Tabs />
    
           <img src={logo} className="App-logo" alt="logo" />
          
